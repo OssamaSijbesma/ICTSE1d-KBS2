1fadd09fa640cd2d4fb91b787a92b8e29a8be2d4
Jeffrey Norder <norderjeffrey@gmail.com>
2018-11-20 12:04:01 +0100

    Test 1

---
+++ PiaNotes/Databaser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace PiaNotes
+{
+    class Databaser
+    {
+        const string ConnectionString = "SERVER = DESKTOP-PA2O8G9; DATABASE = pianotes; USER ID = root; PASSWORD = ";
+
+        private void Connect()
+        {
+            using (SqlConnection) { }
+        }
+    }
+}
