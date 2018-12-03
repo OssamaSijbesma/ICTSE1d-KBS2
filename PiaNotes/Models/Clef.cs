3dbba8123a32ffe148346bec78cd2aa7e4e1b7e5
Ossama Sybesma <ossama_sybesma@hotmail.com>
2018-12-03 13:35:46 +0100

    Added clef and staff

---
+++ PiaNotes/Models/Clef.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiaNotes.Models
+{
+    class Clef
+    {
+        enum ClefTypes { Treble, Bass, Alto, Tenor}
+
+        private ClefTypes name;
+    }
+}
---
+++ PiaNotes/Models/Staff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiaNotes.Models
+{
+    class Staff
+    {
+        private int lines = 5;
+    }
+}
